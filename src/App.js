9adcb1df075bfe211d082057703f5ed0db8d3cad
miok-jung <miok.dev@gmail.com>
2022-03-08 14:39:49 +0900

    study: How to used fetch in React

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (e) => {
-    setToDo(e.target.value);
-  };
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          type="text"
-          value={toDo}
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, id) => {
-          return (
-            <>
-              <li key={id}>{item}</li>
-              <button>Delete</button>
-              <button>Modify</button>
-            </>
-          );
-        })}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
